6250fdb6744aaef8fcf641e392844b03fb88a131
rebecca.la <rebecca.la@ui.ac.id>
2018-10-31 20:56:28 +0700

    finish

--- src/main/java/com/apap/tutorial7/controller/ModelController.java
+++ src/main/java/com/apap/tutorial7/controller/ModelController.java
@@ -3,7 +3,6 @@ package com.apap.tutorial7.controller;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
